569858c81ff71ac871838a62f38fcbe7624f34b7
Ray Zhou <rzhou186@stanford.edu>
2013-03-07 22:24:51 -0800

    more javascript revisions

--- public/javascripts/cricketscorer.js
+++ public/javascripts/cricketscorer.js
@@ -7,30 +7,48 @@
 
 $(document).ready(function(){
 
- 	$("#start-btn").click(function(){
- 		
- 		// Remove subhead window from DOM
- 		$(".subhead-window").remove();
-
+ 	$(".next-btn").click(function(){
  		// Increment currStep, then launch the next step.
  		currStep++;
  		launchStep(currStep);
-
  	});
 
- });
+});
 
+/*
+ * Function: launchStep
+ * ----------------------------------------
+ * Launches next function, given current step.
+ * Triggered by ".next-btn" button.
+ * 
+ */
 function launchStep(step){
+
 	if (step === 1) {
+
+		// Remove subhead window from DOM
+ 		$(".subhead-window").remove();
+
+ 		// Update app window contents
 		$("#step-zero").remove();
 		$("#step-one").css("display", "block");
+
 	}
+
 	else if (step === 2) {
+
+		// Update app window contents
 		$("#step-one").remove();
 		$("#step-two").css("display", "block");	
+
 	}
+
 	else if (step === 3) {
+
+		// Update app window contents
 		$("#step-two").remove();
 		$("#step-three").css("display", "block");		
+
 	}
+
 }
