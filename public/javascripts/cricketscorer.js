19ca7be947657b341bd36c0aba484fcd453dc931
Shubham Goel <gshubham@dn0a2088b5.sunet>
2013-03-07 23:48:16 -0800

    Added step 1

--- public/javascripts/cricketscorer.js
+++ public/javascripts/cricketscorer.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
 
  	$(".next-btn").click(function(){
  		// Increment currStep, then launch the next step.
+ 		checkifValid(currStep);
  		currStep++;
  		launchStep(currStep);
  	});
@@ -44,7 +45,6 @@ function launchStep(step){
 	}
 
 	else if (step === 3) {
-
 		// Update app window contents
 		$("#step-two").remove();
 		$("#step-three").css("display", "block");		
@@ -52,3 +52,46 @@ function launchStep(step){
 	}
 
 }
+
+function checkifValid(step) {
+	if(step === 1)
+	{
+		teamOne.Name = $("#team1-name").val;
+		teamTwo.Name = $("#team2-name").val;
+		if($("#team1-radio").attr("checked") != "undefined") {
+		currBatting = teamOne;
+		}
+		else {
+		currBatting = teamTwo;
+		}
+		if($(teamOne.Name === "") {
+			alert ("Team name one is not filled yet");
+		}
+		else if($(teamTwo.Name === "") {
+			alert ("Team name two is not filled yet");
+		}
+		else if($("#team1-radio").attr("checked") == "undefined" && $("#team2-radio").attr("checked") == "undefined") {
+			alert ("Select atleast one of the radio buttons");
+		}
+	}
+	else if(step === 2) {
+		$(".teamName").val(currBatting.Name);
+		$(".teamScore").val(currBatting.score);
+		$(".teamWickets").val(currBatting.wickets);
+	}
+}
+
+
+/*----------------------- Step Functions ----------------------------------*/
+
+function stepOne() {
+
+		// Remove subhead window from DOM
+ 		$(".subhead-window").remove();
+ 		// Update app window contents
+		$("#step-zero").remove();
+		$("#step-one").css("display", "block");
+}
+
+
+
--- public/javascripts/variables.js
+++ public/javascripts/variables.js
@@ -8,14 +8,21 @@
 
 var currStep = 0;
 
+var currBatting;
+
+
 var teamOne = new Object();
-teamOne.name = "";
+teamOne.Name = "";
 teamOne.bowlers = new Array();
 teamOne.batsmen = new Array();
 teamOne.score = 0;
+teamOne.wickets = 0;
+teamOne.numBalls = 0;
 
 var teamTwo = new Object();
-teamTwo.name = "";
+teamTwo.Name = "";
 teamTwo.bowlers = new Array();
 teamTwo.batsmen = new Array();
 teamTwo.score = 0;
+teamTwo.wickets = 0;
+teamTwo.numBalls = 0;
