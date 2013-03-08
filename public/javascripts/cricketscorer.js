b7c82985c7893a00a63669370bc32bafa847b7b5
Ray Zhou <rzhou186@stanford.edu>
2013-03-07 22:13:42 -0800

    set up javascript base

--- public/javascripts/cricketscorer.js
+++ public/javascripts/cricketscorer.js
@@ -5,6 +5,32 @@
  * 
  */
 
- $(document).ready(function(){
+$(document).ready(function(){
 
- }
+ 	$("#start-btn").click(function(){
+ 		
+ 		// Remove subhead window from DOM
+ 		$(".subhead-window").remove();
+
+ 		// Increment currStep, then launch the next step.
+ 		currStep++;
+ 		launchStep(currStep);
+
+ 	});
+
+ });
+
+function launchStep(step){
+	if (step === 1) {
+		$("#step-zero").remove();
+		$("#step-one").css("display", "block");
+	}
+	else if (step === 2) {
+		$("#step-one").remove();
+		$("#step-two").css("display", "block");	
+	}
+	else if (step === 3) {
+		$("#step-two").remove();
+		$("#step-three").css("display", "block");		
+	}
+}
---
+++ public/javascripts/variables.js
@@ -0,0 +1,21 @@
+/*
+ * variables.js
+ * ----------------------------------------
+ * Initializes and tracks variables for web application.
+ * 
+ */
+
+
+var currStep = 0;
+
+var teamOne = new Object();
+teamOne.name = "";
+teamOne.bowlers = new Array();
+teamOne.batsmen = new Array();
+teamOne.score = 0;
+
+var teamTwo = new Object();
+teamTwo.name = "";
+teamTwo.bowlers = new Array();
+teamTwo.batsmen = new Array();
+teamTwo.score = 0;
