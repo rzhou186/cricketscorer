a3516ca8989a47e2f2c28a9d3c565c48c952a65e
gshubham <shubhamgoel123@gmail.com>
2013-03-21 17:59:57 -0700

    Trying stuff with mail2

--- public/javascripts/mailto.js
+++ public/javascripts/mailto.js
@@ -9,10 +9,10 @@ function initContacts(email){
 	$('#contact-mailto').attr('href', 'mailto:?Body=Please%20provide%20a%20link%20and%20description%20of%20your%20suggestion.');
 }
 
-function emailScorecard(scorecard){
+function emailScorecard(scorecard) {
 
-	var emailBody = '';
-
-	$('#emailScorecard').attr('href', 'mailto:?Body=' + emailBody);
+	scorecard.replace(/ /g, "%20");
+	scorecard.replace(/\n/g, "%0A");
+	$('#emailScorecard').attr('href', 'mailto:?Body=' + scorecard);
 
 }
